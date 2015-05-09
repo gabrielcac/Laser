c1f6ff293424a64bfeb972cacbca3b15f5152649
Gabriel Coutinho <gabrielcac@gmail.com>
2015-05-09 12:20:53 -0300

    Creating repository

---
+++ Assets/LaserShot/Shoot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public Vector3 localShootStart;
+	public GameObject laserPrefab;
+	public GameObject world;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKeyDown)
+		{
+			ShootLaser();
+		}
+	}
+
+	private void ShootLaser()
+	{
+		GameObject laser = (GameObject) GameObject.Instantiate(laserPrefab);
+		laser.transform.position = transform.localToWorldMatrix * localShootStart;
+		laser.GetComponent<SpringJoint>().connectedBody = world.rigidbody;
+		laser.rigidbody.velocity = transform.rotation * (10f * Vector3.forward);
+	}
+}
